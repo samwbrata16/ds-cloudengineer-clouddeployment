03c3947a3f49c543e54b7ef49978fffa2fe5b4ad
samwbrata16 <swbrata93@gmail.com>
2026-03-24 14:24:02 +0700

    commit

--- test/app.test.js
+++ test/app.test.js
@@ -18,3 +18,11 @@ describe("POST /api/generate", () => {
     expect(typeof res.body.text).toBe("string");
   });
 });
+
+describe("GET /health", () => {
+  it("should return OK", async () => {
+    const res = await request(app).get("/health");
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe("OK");
+  });
+});
