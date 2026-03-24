4773e4ebccb9d6097125f8e70e5fece89505e255
samwbrata16 <swbrata93@gmail.com>
2026-03-24 16:39:10 +0700

    commit

--- server.js
+++ server.js
@@ -5,3 +5,11 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM signal received: closing HTTP server');
+  server.close(() => {
+    console.log('HTTP server closed');
+    process.exit(0);
+  });
+});
